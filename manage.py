44ff03710ab0fd420c67a4073d424b6a627e01e9
Matt Frost <mwfrost@fastmail.fm>
2020-10-06 14:04:33 -0700

    Add schemas and preliminary JSON querying

--- manage.py
+++ manage.py
@@ -13,6 +13,7 @@ def create_db():
 
 @cli.command("load_seed_data")
 def load_seed_data():
+    # https://chem.nlm.nih.gov/chemidplus/rn/1050-79-9
     c1 = Compound(
         id="DTXCID302000003",
         identifiers='{ "preferred_name":"Moperone","casrn":"1050-79-9","inchikey": "AGAHNABIDCTLHW-UHFFFAOYSA-N", "casalts":[{"casalt":"0001050799","weight:0.5},{"casalt":"1050799","weight":0.5}],"synonyms": [{"synonym": "Meperon","weight": 0.75},{"synonym": "Methylperidol","weight": 0.5}]}',
